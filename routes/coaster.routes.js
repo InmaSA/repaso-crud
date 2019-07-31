17fe152ac75827022246152dcec2641f5e8ee394
Inma <inmanewmail@gmail.com>
2019-07-31 14:20:51 +0200

    done pero sin el bonus

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -28,8 +28,21 @@ router.get('/', (req, res, next) => {
   Coaster.find()
   .populate('park_id')
   .then(allTheCoasters => res.render('coasters/coasters-index', {allTheCoasters}))
+  .catch(err => {
+    console.log('Error al acceder al mandar los datos del populate a la view de coasters-index', err)
+    next()  
+  })
+})  
+
+router.get('/:id', (req, res, next) => {
+  const coasterId = req.params.id
+  Coaster.findById(coasterId)
+  .populate('park_id')
+  .then(eachCoaster => res.render('coasters/coaster-details', {eachCoaster}))
+  .catch(err => {
+    console.log('Error al acceder al acceder a la view de coaster-details con el id de su parque', err)
+    next()  
+  })
 })
-
-
 
 module.exports = router
